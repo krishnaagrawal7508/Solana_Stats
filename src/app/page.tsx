67654e805eaab34c6fa3ef2c2a9b0d9b65f28bbc
krishna agrawal <agrawalkrishna522@gmail.com>
2024-09-14 20:01:27 +0530

    vercel speed insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const TransactionsHeatmap: React.FC = () => {
   return (
     <div>
       <p >Solana Stats</p>
+      <SpeedInsights />
     </div>
   );
 };
